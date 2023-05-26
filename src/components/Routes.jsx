d74d4c88ff2001ab1cf08b69b2e4ee1ee2cbd157
erfan.pirikhah <erfan.pirikhah1@gmail.com>
2023-05-26 18:52:17 +0330

    add tab drageable

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -3,6 +3,8 @@ import { Route, Routes as Switch } from "react-router-dom";
 import Home from "../pages/Home";
 import Layout from "./layout/Layout";
 import Component from "../pages/Tabs";
+import Tab2 from "../pages/Tabs2";
+import Tab3 from "../pages/Tabs3";
 
 
 const Routes = () => {
@@ -13,6 +15,8 @@ const Routes = () => {
       <Switch>
         <Route path="/" exact Component={Home} />
         <Route path="/tabs" Component={Component} />
+        <Route path="/tabs2" Component={Tab2} />
+        <Route path="/tabs3" Component={Tab3} />
       </Switch>
     </Layout>
   );
