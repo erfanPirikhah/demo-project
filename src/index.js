560e3696893393692e0f1226491eae26a7c9a6c9
erfan.pirikhah <erfan.pirikhah1@gmail.com>
2023-06-21 21:23:54 +0330

    add word editor

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { ConfigProvider } from "antd";
+import {registerLicense} from '@syncfusion/ej2-base';
+registerLicense("Mgo+DSMBMAY9C3t2VVhjQlFac1lJXGFWf1NpR2NbfU54flBAalhWVAciSV9jS3xSd0dgWX9fc3dSR2JUUA==")
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
